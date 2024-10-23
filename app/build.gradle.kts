b9b2584637ad5c30492860f6d1d63537542b4f95
stefan@stefanhorvath.at <stefan@stefanhorvath.at>
2024-10-23 23:06:56 +0200

    First commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.test.kotlinserver"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.test.kotlinserver"
+        minSdk = 34
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        val kotlin_version = "1.9.21"
+
+        jvmTarget = "1.8"
+        version = kotlin_version
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+
+    packaging {
+        resources {
+            excludes += listOf("/META-INF/{AL2.0,LGPL2.1}", "META-INF/INDEX.LIST", "META-INF/io.netty.versions.properties")  }
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    //androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    val ktor_version = "1.6.7"
+    implementation("io.ktor:ktor-server-core:$ktor_version")
+    implementation("io.ktor:ktor-server-netty:$ktor_version")
+    implementation("io.ktor:ktor-features:$ktor_version")
+
+    implementation("androidx.appcompat:appcompat:1.7.0")
+
+    implementation("io.ktor:ktor-client-core:1.6.7")
+    implementation("io.ktor:ktor-client-android:1.6.7")
+    implementation("io.ktor:ktor-client-json:1.6.7")
+    implementation("io.ktor:ktor-client-serialization:1.6.7")
+}
---
+++ app/src/main/java/com/test/kotlinserver/MainActivity.kt
@@ -0,0 +1,198 @@
+package com.test.kotlinserver
+
+import android.app.Application
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.test.kotlinserver.ui.theme.KotlinServerTheme
+import io.ktor.application.*
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.features.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.util.pipeline.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.io.FileInputStream
+import java.security.KeyStore
+import java.time.Duration
+import java.util.*
+
+
+val client = HttpClient()
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            KotlinServerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+        GlobalScope.launch {
+            startWebAPI("127.0.0.1:8080", false, "", "", "", Duration.ofSeconds(10), Duration.ofSeconds(10), UUID.fromString("00000000-0000-0000-0000-000000000000"))
+        }
+    }
+    @Composable
+    fun Greeting(name: String) {
+
+        val client = HttpClient()
+
+        val coroutineScope = rememberCoroutineScope()
+        var text by remember { mutableStateOf("No data yet") }
+
+        val onClick: () -> Unit = {
+            coroutineScope.launch {
+                try {
+                    val response: HttpStatement = client.get("http://127.0.0.1:8080/test")
+                    val textResponse: String = response.receive<String>()
+
+                    // Update the UI on the main thread
+                    withContext(Dispatchers.Main) {
+                        text = textResponse
+                    }
+                } catch(e: Exception) {
+                    // Handle the error
+                    withContext(Dispatchers.Main) {
+                        text = "Error: ${e.message}"
+                    }
+                } finally {
+                    client.close()
+                }
+            }
+        }
+        Column {
+            Text(text = text) // Display the fetched text here
+            Button(
+                onClick = onClick,
+                modifier = Modifier.width(200.dp).height(50.dp)
+            ) {
+                Text(text = "Click Me")
+            }
+        }
+    }
+
+    @Preview(showBackground = true)
+    @Composable
+    fun GreetingPreview() {
+        KotlinServerTheme {
+            Greeting("Android")
+        }
+    }
+
+    suspend fun startWebAPI(
+        webListen: String,
+        useSSL: Boolean,
+        certificateFile: String,
+        certificateKey: String,
+        info: String,
+        readTimeout: Duration,
+        writeTimeout: Duration,
+        APIKey: UUID
+    ) {
+
+
+
+        println("Starting API at '$webListen'")
+        fun Application.registerRoutes() {
+
+        }
+
+        val environment = applicationEngineEnvironment {
+            module {
+                install(CallLogging)
+                install(Routing) {
+                    route("/test") {
+                        get { apiTest() }
+                    }
+                    route("/status") {
+                        get { apiStatus() }
+                        route("/peers") {
+                            get { apiStatusPeers() }
+                        }
+                        route("/config") {
+                            get { apiStatusConfig() }
+                        }
+                    }
+                    route("/account") {
+                        route("/info") {
+                            get { apiAccountInfo() }
+                        }
+                        route("/delete") {
+                            get { apiAccountDelete() }
+                        }
+                    }
+                }
+            }
+            if (useSSL) {
+                val keystore: KeyStore = KeyStore.getInstance("PKCS12") // or "PKCS12" for .p12 files
+                val fileInputStream: FileInputStream = FileInputStream(certificateFile)
+                keystore.load(fileInputStream, certificateKey.toCharArray())
+                sslConnector(
+                    keyStore = keystore,
+                    keyAlias = "alias",
+                    keyStorePassword = { certificateKey.toCharArray() },
+                    privateKeyPassword = { certificateFile.toCharArray() }
+                ) {
+                    this.host = "127.0.0.1"
+                    this.port = 8080
+                }
+            } else {
+                connector {
+                    this.host = "127.0.0.1"
+                    this.port = 8080
+                }
+            }
+        }
+
+        val server = embeddedServer(Netty, environment)
+        server.start(wait = true)
+    }
+    suspend fun PipelineContext<Unit, ApplicationCall>.apiTest() {
+        // Implement function here, for testing you can just return a simple string
+        call.respondText("API Test Successful")
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.apiStatus() {
+        // Implement your function here
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.apiStatusPeers() {
+        // Implement your function here
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.apiStatusConfig() {
+        // Implement your function here
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.apiAccountInfo() {
+        // Implement your function here
+    }
+
+    suspend fun PipelineContext<Unit, ApplicationCall>.apiAccountDelete() {
+        // Implement your function here
+    }
+}
